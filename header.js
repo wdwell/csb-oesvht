cff6c9c80f88da2e85ef08aaebbdffb80c1f78f7
wdwell <mailwdwell@yandex.ru>
2022-12-07 13:53:59 +0600

    07.12
    h1 - done
    add 320 dpi
    auto format

---
+++ header.js
@@ -0,0 +1,19 @@
+let lastScroll = 0;
+const defaultOffset = 20;
+const header = document.querySelector('.sticky');
+
+const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
+const containHide = () => header.classList.contains('hide');
+
+window.addEventListener('scroll', () => {
+    if( scrollPosition() > defaultOffset) {
+        //scroll down
+        header.classList.add('hide');
+    }
+    else if(scrollPosition() < lastScroll && containHide()){
+        //scroll up
+        header.classList.remove('hide');
+    }
+
+    lastScroll = scrollPosition();
+})
